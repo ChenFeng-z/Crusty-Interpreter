0ef5083143deab951c0dc86993a47182bb937701
ChenFeng-z <896512290@qq.com>
2026-06-25 11:58:02 +0800

    Some initial work on project 3

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -1,14 +1,79 @@
 //tokenize.rs
 
+use std::vec;
+
 use crate::reader::Source;
 
-pub struct  Tokens { }
+#[derive(Debug, PartialEq)]
+pub enum TokenType {
+    // Single-character tokens
+    TLeftParen,
+    TRightParen,
+    TLeftBrace,
+    TRightBrace,
+    TComma,
+    TDot,
+    TMinus,
+    TPlus,
+    TSemiColon,
+    TSlash,
+    TStar,
+
+    // One or two character tokens
+    TBang,
+    TBangEqual,
+    TEqual,
+    TEqualEqual,
+    TGreater,
+    TGreaterEqual,
+    TLess,
+    TLessEqual,
+
+    // Literals
+    TIdentifier,
+    TString,
+    TNumber,
+
+    // Keywords
+    TAnd,
+    TClass,
+    TElse,
+    TFalse,
+    TFun,
+    TFor,
+    TIf,
+    TNil,
+    TOr,
+    TPrint,
+    TReturn,
+    TSuper,
+    TThis,
+    TTrue,
+    TVar,
+    TWhile,
+
+    TEof,
+}
+
+use TokenType::*;
+
+#[derive(Debug)]
+pub struct Token {
+    pub tokentype: TokenType,
+    pub lexeme: String,
+    pub line: usize,
+}
+#[derive(Debug)]
+pub struct  Tokens { 
+    pub tokens: Vec<Token>,
+}
 #[derive(Debug)]
 pub struct  Error { }
 
 pub fn tokenize(_source :Source) -> Result<Tokens, Error> {
     println!("Tokenizing");
-    Ok(Tokens {  })
+    let tokens = vec![];
+    Ok(Tokens { tokens })
 }
 
 #[cfg(test)]
